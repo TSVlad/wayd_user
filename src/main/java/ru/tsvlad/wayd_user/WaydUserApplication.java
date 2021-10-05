59f2003ac8232efa90f8e46daee768c552c2613b
TSVlad <vladislav.kolmykov@yandex.ru>
2021-10-05 21:45:10 +0300

    eureka client added

--- src/main/java/ru/tsvlad/wayd_user/WaydUserApplication.java
+++ src/main/java/ru/tsvlad/wayd_user/WaydUserApplication.java
@@ -2,8 +2,10 @@ package ru.tsvlad.wayd_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class WaydUserApplication {
 
     public static void main(String[] args) {
