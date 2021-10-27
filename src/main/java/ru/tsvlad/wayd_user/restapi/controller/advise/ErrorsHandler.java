5c09b32630d6b19dacf9eaf87de42fa800d54b1a
TSVlad <vladislav.kolmykov@yandex.ru>
2021-10-27 09:26:45 +0300

    Controller advice updated

--- src/main/java/ru/tsvlad/wayd_user/restapi/controller/advise/ErrorsHandler.java
+++ src/main/java/ru/tsvlad/wayd_user/restapi/controller/advise/ErrorsHandler.java
@@ -4,13 +4,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.tsvlad.wayd_user.restapi.controller.advise.exceptions.UnsuccessfulLoginException;
+import ru.tsvlad.wayd_user.restapi.controller.advise.exceptions.*;
 
 @ControllerAdvice
 public class ErrorsHandler {
 
     @ExceptionHandler(value = {UnsuccessfulLoginException.class})
-    protected ResponseEntity<String> handleUnsuccessfulLogin() {
-        return new ResponseEntity<>("", HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<String> handleUnsuccessfulLogin(Exception e) {
+        return new ResponseEntity<>(getExceptionName(e), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(value = {EmailAlreadyExistsException.class, UsernameAlreadyExistsException.class})
+    public ResponseEntity<String> handleEmailAlreadyExists(Exception e) {
+        return new ResponseEntity<>(getExceptionName(e), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = {ForbiddenException.class})
+    public ResponseEntity<String> handleForbidden(Exception e) {
+        return new ResponseEntity<>(getExceptionName(e), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ResponseEntity<String> handleNotFound(Exception e) {
+        return new ResponseEntity<>(getExceptionName(e), HttpStatus.NOT_FOUND);
+    }
+
+    private String getExceptionName(Exception e) {
+        String [] exceptionWithPackages = e.getClass().getName().split("\\.");
+        return exceptionWithPackages[exceptionWithPackages.length - 1];
     }
 }
