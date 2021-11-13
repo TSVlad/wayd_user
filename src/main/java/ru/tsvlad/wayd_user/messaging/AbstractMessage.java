06b8bc474220bf5ab455c7f2ca1766602e5974c1
TSVlad <vladislav.kolmykov@yandex.ru>
2021-11-13 21:59:37 +0300

    abstract message fix for notification

--- src/main/java/ru/tsvlad/wayd_user/messaging/AbstractMessage.java
+++ src/main/java/ru/tsvlad/wayd_user/messaging/AbstractMessage.java
@@ -3,6 +3,7 @@ package ru.tsvlad.wayd_user.messaging;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import ru.tsvlad.wayd_user.restapi.dto.JwtPayload;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -15,6 +16,8 @@ public abstract class AbstractMessage implements Serializable {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime created;
+
+    private JwtPayload userInfo;
 
     public AbstractMessage() {
         this.created = LocalDateTime.now();
