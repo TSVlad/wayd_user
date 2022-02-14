8d33e49a11381b54de970ec318ec727ba34bb31e
TSVlad <vladislav.kolmykov@yandex.ru>
2022-02-14 19:50:20 +0300

    keycloak refactor

--- src/main/java/ru/tsvlad/wayd_user/messaging/AbstractMessage.java
+++ src/main/java/ru/tsvlad/wayd_user/messaging/AbstractMessage.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import ru.tsvlad.wayd_user.commons.UserInfo;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -16,6 +17,8 @@ public abstract class AbstractMessage implements Serializable {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime created;
+
+    private UserInfo userInfo;
 
     public AbstractMessage() {
         this.created = LocalDateTime.now();
