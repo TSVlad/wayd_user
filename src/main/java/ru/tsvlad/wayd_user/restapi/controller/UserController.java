f30a616ff403df6d7cb5e5cc08413dd4c39f8835
tsvlad <vladislav.kolmykov@yandex.ru>
2022-03-28 20:16:01 +0300

    swagger docks added

--- src/main/java/ru/tsvlad/wayd_user/restapi/controller/UserController.java
+++ src/main/java/ru/tsvlad/wayd_user/restapi/controller/UserController.java
@@ -1,5 +1,9 @@
 package ru.tsvlad.wayd_user.restapi.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -32,6 +36,12 @@ public class UserController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+            description = "Registering user in system and sending him email for mail confirmation",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Object with new user's info"
+            )
+    )
     public UserDTO registerUser(@RequestBody UserRegisterDTO userDTO) {
         log.debug("Register user request gotten");
         return modelMapper.map(
@@ -41,6 +51,12 @@ public class UserController {
 
     @Secured("ROLE_MODERATOR")
     @PostMapping("/register/organization")
+    @Operation(
+            description = "Registering organization in system and sending email for mail confirmation",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Object with new organization's info"
+            )
+    )
     public UserDTO registerOrganization(@RequestBody OrganizationRegisterDTO organizationRegisterDTO) {
         log.debug("Register organization request gotten for {}", organizationRegisterDTO);
         return modelMapper.map(
@@ -52,6 +68,12 @@ public class UserController {
     @Secured("ROLE_USER")
     @PutMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @Operation(
+            description = "Updating user info in system",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Object with updated user's info"
+            )
+    )
     public UserDTO updateUser(@RequestBody UserForUpdateDTO userForUpdateDTO,
                               Authentication authentication) {
         log.debug("Update user request gotten: {}", userForUpdateDTO);
@@ -66,6 +88,14 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(
+            description = "Getting page with users by text",
+            parameters = {
+                    @Parameter(name = "searchString", description = "String to search users by name, surname, username, etc"),
+                    @Parameter(name = "page", description = "Page number"),
+                    @Parameter(name = "size", description = "Page size")
+            }
+    )
     public Page<UserPublicDTO> getAll(@RequestParam(required = false, defaultValue = "") String searchString, @RequestParam int page, @RequestParam int size) {
         log.debug("Get all users request gotten: page {}, size{}", page, size);
         return userService.getAllActiveByUsername(searchString, page, size)
@@ -73,6 +103,12 @@ public class UserController {
     }
 
     @PostMapping("/by-ids")
+    @Operation(
+            description = "Getting users by ids",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "List of users' ids"
+            )
+    )
     public List<UserPublicDTO> getAllByIds(@RequestBody List<String> ids) {
         log.debug("Get all users by ids request gotten: ids {}", ids);
         return userService.getAllByIds(ids).stream()
@@ -81,6 +117,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            description = "Getting user by id",
+            parameters = {
+                    @Parameter(name = "id", description = "User's id")
+            }
+    )
     public UserPublicDTO getUserById(@PathVariable String id) {
         log.debug("Get user by id request gotten: id {}", id);
         return modelMapper.map(userService.getUserById(id), UserPublicDTO.class);
